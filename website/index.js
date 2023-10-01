35cc6caa5e9f4a37201786588794252b9bdf18e3
Fatih <f.abdioglu52@gmail.com>
2023-10-01 19:26:18 +0300

    html and css updated

--- website/index.js
+++ website/index.js
@@ -1,10 +1,10 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://qmnsdehd7srfmes4to2apadiyi0vwxfz.lambda-url.us-east-1.on.aws/"
+        "https://fbgkf6pgsr4fxc5dr3nw5c2jmu0pnrln.lambda-url.us-east-1.on.aws/"
     );
     let data = await response.json();
-    counter.innerHTML = `👀 Total visitors: ${data}`;
+    counter.innerHTML = `Total visitors: ${data}`;
 }
 
 updateCounter();
